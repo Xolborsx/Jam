e0648ecb255557fc80d2f79e488caab3aa417973
Xolborsx <lucasmaurent91@gmail.com>
2026-06-18 16:34:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let x = 200;
 let y = 200;
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowRight") x += 10;
-    if (e.key === "ArrowLeft") x -= 10;
-    if (e.key === "ArrowUp") y -= 10;
-    if (e.key === "ArrowDown") y += 10;
+    if (e.key === "d") x += 10;
+    if (e.key === "a") x -= 10;
+    if (e.key === "w") y -= 10;
+    if (e.key === "s") y += 10;
 
     jogador.style.left = x + "px";
     jogador.style.top = y + "px";
