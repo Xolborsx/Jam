d0d633e8d983b540a76794d4f78d1462fce169e5
Xolborsx <lucasmaurent91@gmail.com>
2026-06-18 17:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 const jogador = document.getElementById("jogador");
+const moeda = document.getElementById("moeda");
+const pontosTexto = document.getElementById("pontos");
 
-let x = 200;
-let y = 200;
+let x = 0;
+let y = 0;
+let pontos = 0;
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "d") x += 10;
-    if (e.key === "a") x -= 10;
-    if (e.key === "w") y -= 10;
-    if (e.key === "s") y += 10;
 
-    jogador.style.left = x + "px";
-    jogador.style.top = y + "px";
+    if (e.key === "ArrowRight") x += 10;
+    if (e.key === "ArrowLeft") x -= 10;
+    if (e.key === "ArrowUp") y -= 10;
+    if (e.key === "ArrowDown") y += 10;
 
-    const jogador = document.getElementById("jogador");
-
-let x = 200;
-let y = 200;
-
-const larguraTela = window.innerWidth;
-
-
-    // Limites
+    // Bordas da arena
     if (x < 0) x = 0;
     if (y < 0) y = 0;
-
-    if (x > larguraTela - 50) x = larguraTela - 50;
-    if (y > alturaTela - 50) y = alturaTela - 50;
+    if (x > 560) x = 560;
+    if (y > 360) y = 360;
 
     jogador.style.left = x + "px";
     jogador.style.top = y + "px";
-  
+
+    verificarColisao();
 });
+
+function verificarColisao() {
+
+    const jogadorRect = jogador.getBoundingClientRect();
+    const moedaRect = moeda.getBoundingClientRect();
+
+    if (
+        jogadorRect.left < moedaRect.right &&
+        jogadorRect.right > moedaRect.left &&
+        jogadorRect.top < moedaRect.bottom &&
+        jogadorRect.bottom > moedaRect.top
+    ) {
+
+        pontos++;
+        pontosTexto.textContent = pontos;
+
+        const novaX = Math.random() * 550;
+        const novaY = Math.random() * 350;
+
+        moeda.style.left = novaX + "px";
+        moeda.style.top = novaY + "px";
+    }
+}
